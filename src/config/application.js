6308ea5ae818e47bb6001691d8401c0dd0a9607c
andresalejandro.juarez@externos-cl.cencosud.com <andresalejandro.juarez@externos-cl.cencosud.com>
2019-07-20 14:18:42 -0400

    Adding CORS config

--- src/config/application.js
+++ src/config/application.js
@@ -1,6 +1,18 @@
 const bodyParser = require('body-parser')
 
 module.exports = (app) => {
+  const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+      res.send(200)
+    } else {
+      next()
+    }
+  }
   const logRequestStart = (req, res, next) => {
     console.info(`${req.method} ${req.originalUrl}`)
     res.on('finish', () => {
@@ -8,6 +20,7 @@ module.exports = (app) => {
     })
     next()
   }
+  app.user(allowCrossDomain)
   app.use(logRequestStart)
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
